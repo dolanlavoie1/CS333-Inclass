1d92ac94516c31db0c1c6ccd9a74178cf3c8242c
Shane K. Panter <shanepanter@boisestate.edu>
2026-02-02 11:40:55 -0700

    in class updates

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
-# Ping sweep the Lab
 
-base="onyxnode"
 
-for q in {1..200}
-do
-	curr="$base$q"
-	ping -c 1 $curr >> ping.log
-done
+# Prints usage information
+function usage() {
+	echo "Usage: $0"
+	exit 1
+}
 
+# Pingsweep command for the Onyx nodes
+function pingsweep_cmd() {
+	local base="onyxnode"
+	echo "Pinging nodes... TBD"
+}
+
+function main() {
+	while getopts ":hp" opt; do
+		case ${opt} in
+		h) usage ;;
+		p) pingsweep_cmd ;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			usage
+			;;
+		esac
+	done
+	shift $((OPTIND -1))
+}
+
+##  Script entry point
+if [ $# -eq 0 ]; then
+	usage
+else
+	main "$@"
+fi
